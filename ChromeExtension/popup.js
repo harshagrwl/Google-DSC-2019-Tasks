ac175ac97c0c23fcc6a3bb570b81c9a63a8cd144
Harsh Agarwal <agrawal.harsh14@gmail.com>
2019-08-24 09:23:48 +0530

    [ChromeExtension]: Added Popup files with basic UI

---
+++ ChromeExtension/popup.js
@@ -0,0 +1,26 @@
+function sendMessage() {
+    chrome.runtime.sendMessage({action: "getCurrentForecast"}, function(response) {
+        if (response == null) {
+            setTimeout(sendMessage, 2000);
+        } else {
+            showResult(response);
+        }
+    });
+}
+
+function showResult(response) {
+    var statusDiv = document.getElementById("status");
+    var icoImg = document.getElementById("ico");
+    var temperatureDiv = document.getElementById("temperature");
+
+    statusDiv.textContent = response.currently.summary;
+
+    temperatureDiv.style.display = "block";
+    temperatureDiv.textContent = response.currently.temperature.toString().split('.')[0] + "˚C";
+
+    icoImg.style.display = "block";
+    icoImg.src = chrome.extension.getURL("/" + response.currently.icon + ".png");
+
+}
+
+document.addEventListener('DOMContentLoaded', sendMessage);
